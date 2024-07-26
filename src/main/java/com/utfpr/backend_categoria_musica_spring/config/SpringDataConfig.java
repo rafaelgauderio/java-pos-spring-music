d0680860eaf7f98f1ab2ed1a688dba4d1ff1cba1
Rafael de Luca <deluca1712@gmail.com>
2024-07-26 16:51:58 -0300

    implementing programmatic configuration

---
+++ src/main/java/com/utfpr/backend_categoria_musica_spring/config/SpringDataConfig.java
@@ -0,0 +1,58 @@
+package com.utfpr.backend_categoria_musica_spring.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories("com.utfpr.backendCategoriaMusicaSpringApplication.repository")
+@EnableTransactionManagement
+public class SpringDataConfig {
+
+    @Bean
+    public DataSource dataSource () {
+
+        HikariDataSource dataSource = new HikariDataSource();
+
+        dataSource.setUsername("root");
+        dataSource.setPassword("senharoot");
+        dataSource.setJdbcUrl("jdbc:h2:men:test");
+        dataSource.setDriverClassName("org.h2.Driver");
+        return dataSource;
+    }
+
+    @Bean
+    public EntityManagerFactory entityManagerFactory () {
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter ();
+        vendorAdapter.setGenerateDdl(true);
+        vendorAdapter.setShowSql(true);
+
+        factoryBean.setDataSource(dataSource());
+        factoryBean.setJpaVendorAdapter(vendorAdapter);
+        factoryBean.setPackagesToScan("com.utfpr.backendCategoriaMusicaSpringApplication.entity");
+        factoryBean.afterPropertiesSet();
+
+        return factoryBean.getObject();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager () {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory());
+        transactionManager.setJpaDialect(new HibernateJpaDialect());
+
+        return transactionManager;
+    }
+
+}
