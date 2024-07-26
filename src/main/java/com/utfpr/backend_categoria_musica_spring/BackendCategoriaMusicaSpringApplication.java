4f710998606e2fb4dd5dfffd9d4c9d6604854696
Rafael de Luca <deluca1712@gmail.com>
2024-07-26 15:09:07 -0300

    create spring project and add dependecies

---
+++ src/main/java/com/utfpr/backend_categoria_musica_spring/BackendCategoriaMusicaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.utfpr.backend_categoria_musica_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCategoriaMusicaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCategoriaMusicaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utfpr/backend_categoria_musica_spring/BackendCategoriaMusicaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utfpr.backend_categoria_musica_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCategoriaMusicaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
