259ca7e282acba927690253fcb478de76312767a
Rafael de Luca <deluca1712@gmail.com>
2024-07-30 11:06:56 -0300

    service - method find all musics

---
+++ src/main/java/com/utfpr/backend_categoria_musica_spring/service/MusicaService.java
@@ -0,0 +1,22 @@
+package com.utfpr.backend_categoria_musica_spring.service;
+
+import com.utfpr.backend_categoria_musica_spring.entity.Musica;
+import com.utfpr.backend_categoria_musica_spring.repository.MusicaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MusicaService {
+
+    @Autowired
+    private MusicaRepository musicaRepository;
+
+    public List<Musica> findAllMusics () {
+        List<Musica> musicaList = new ArrayList<>();
+        musicaList = musicaRepository.findAll();
+        return musicaList;
+    }
+}
